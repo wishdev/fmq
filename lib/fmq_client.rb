8c5dcf4ea2a259cd835aa499e97485baaaeffa8c
John W Higgins <wishdev@gmail.com>
2009-07-26 22:26:48 -0700

    Adding patron_client

--- lib/fmq_client.rb
+++ lib/fmq_client.rb
@@ -1,5 +1,6 @@
 #
 # Copyright (c) 2008 Vincent Landgraf
+# Copyright (c) 2009 John W Higgins
 #
 # This file is part of the Free Message Queue.
 #
@@ -25,7 +26,8 @@ module FreeMessageQueue
       :payload, # the content itself
       :created_at, # when came the message into the system
       :content_type, # the content type of the message
-      :option # options hash (META-DATA) for the message
+      :option, # options hash (META-DATA) for the message
+      :valid #signifies if message was actually returned
 
     # Create a message item. The payload is often just a string
     def initialize(payload, content_type = "text/plain", created_at = Time.new)
@@ -41,6 +43,50 @@ module FreeMessageQueue
     # Size of item in bytes
     def bytes
       @payload.size
+    end
+  end
+
+  class BaseClient
+    # create a connection to a queue (by url)
+    def initialize(url)
+      @url = URI.parse(url)
+    end
+
+    def handle_poll(res)
+      message = Message.new(res.body, res["CONTENT-TYPE"])
+
+      message.valid = (res.status == 200)
+      res.each_key do |option_name|
+        if option_name.upcase.match(/MESSAGE_([a-zA-Z][a-zA-Z0-9_\-]*)/)
+          message.option[$1] = res[option_name]
+        end
+      end
+
+      message
+    end
+
+    def put_headers(message)
+      header = {}
+      header["CONTENT-TYPE"] = message.content_type
+
+      # send all options of the message back to the client
+      if message.respond_to?(:option) && message.option.size > 0
+        for option_name in message.option.keys
+          header["MESSAGE_#{option_name}"] = message.option[option_name].to_s
+        end
+      end
+
+      header
+    end
+
+    # return the size (number of messages) of the queue
+    def size
+      head["QUEUE_SIZE"].to_i
+    end
+
+    # return the size of the queue in bytes
+    def bytes
+      head["QUEUE_BYTES"].to_i
     end
   end
 
@@ -70,64 +116,86 @@ module FreeMessageQueue
   #  message = FreeMessageQueue::Message.new(payload, "application/yaml")
   #  queue.put(message)
   #
-  class ClientQueue
-    # create a connection to a queue (by url)
-    def initialize(url)
-      @url = URI.parse(url)
-    end
 
+  class ClientQueue < BaseClient
     # this returns one message from the queue as a string
     def poll()
       res = Net::HTTP.start(@url.host, @url.port) do |http|
         http.get(@url.path)
       end
 
-      message = Message.new(res.body, res["CONTENT-TYPE"])
-
-      res.each_key do |option_name|
-        if option_name.upcase.match(/MESSAGE_([a-zA-Z][a-zA-Z0-9_\-]*)/)
-          message.option[$1] = res[option_name]
-        end
-      end
-
-      return message
+      handle_poll(res)
     end
 
     alias get poll
 
     # this puts one message to the queue
     def put(message)
-      header = {}
-      header["CONTENT-TYPE"] = message.content_type
-
-      # send all options of the message back to the client
-      if message.respond_to?(:option) && message.option.size > 0
-        for option_name in message.option.keys
-          header["MESSAGE_#{option_name}"] = message.option[option_name].to_s
-        end
-      end
-
       Net::HTTP.start(@url.host, @url.port) do |http|
-        http.post(@url.path, message.payload, header)
+        http.post(@url.path, message.payload, put_headers(message))
       end
     end
 
     alias post put
 
-    # return the size (number of messages) of the queue
-    def size
-      head["QUEUE_SIZE"].to_i
-    end
-
-    # return the size of the queue in bytes
-    def bytes
-      head["QUEUE_BYTES"].to_i
-    end
   protected
     # do a head request to get the state of the queue
     def head()
       res = Net::HTTP.start(@url.host, @url.port) do |http|
         http.head(@url.path)
+      end
+    end
+  end
+end
+
+
+begin
+  require 'patron'
+
+  module Patron
+    class Response
+      #adding [] method to ease compatability
+      def [](key)
+        @headers[key]
+      end
+
+      def each_key(&block)
+        @headers.each_key &block
+      end
+    end
+  end
+
+  module FreeMessageQueue
+    class PatronClientQueue < BaseClient
+      # create a connection to a queue (by url)
+      def initialize(url)
+        @sess = Patron::Session.new
+        @sess.base_url = url
+      end
+
+      # this returns one message from the queue as a string
+      def poll(path = '')
+        handle_poll(@sess.get(path))
+      end
+
+      alias get poll
+
+      # this puts one message to the queue
+      def put(message, path = '')
+        @sess.post(path, message.payload, put_headers(message))
+      end
+
+      alias post put
+
+      protected
+      # do a head request to get the state of the queue
+      def head(path = '')
+        sess.head(path)
+      end
+    end
+  rescue LoadError => e
+    module FreeMessageQueue
+      class PatronClient
       end
     end
   end
